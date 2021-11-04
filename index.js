4f94d16a5e0b3bfe2f7a598db4ee0abae4defe99
nicopicchio <nicolapicchio@gmail.com>
2021-11-04 14:04:47 +0000

    working on functions

--- index.js
+++ index.js
@@ -1,54 +1,36 @@
-// Big Mac prices
-let bigMacUS = 5.65;
-let bigMacGB = 4.75;
-let bigMacCH = 7.04;
-let bigMacJAP = 3.55;
-let bigMacEUR = 5.02;
+/*
+Index is calculated by :
+  - Get current US Big Mac price
+  - Get current Big Mac price from country we're comparing
+  - Divide US Big Mac price by country's Big Mac price
+    (this is called the implied purchasing power)
+  - Get country's currency rate to USD value
+  - Substract currency rate with the implied purchasing power
+  - Divide the previous result by the implied purchasing power
+  - This will give you the Big Mac Index
+  - Make sure you search for updated values!
 
-// Currency exchange rates
-let USDUSDIndex = 1.00;
-let GBPUSDIndex = 1.37;
-let CHFUSDIndex = 1.08;
-let JPYUSDIndex = 0.0088;
-let EURUSDIndex = 1.16;
+For example, using figures in July 2008:
+  - the price of a Big Mac was $3.57 in the United States 
+  - the price of a Big Mac was £2.29 in the United Kingdom 
+  - the implied purchasing power parity was $1.56 to £1, that is $3.57/£2.29 = 1.56
+  - this compares with an actual exchange rate of $2.00 to £1 at the time
+  - (2.00 – 1.56) / 1.56 = 28%
+  - the pound was thus overvalued against the dollar by 28%
+*/
 
-// Country IPP
-const USipp = (bigMacUS / bigMacUS);
-const GBipp = (bigMacUS / bigMacGB);
-const CHipp = (bigMacUS / bigMacCH);
-const JAPipp = (bigMacUS / bigMacJAP);
-const EURipp = (bigMacUS / bigMacEUR);
+let USBigMacPrice = 3.99;
+let ITABigMacPrice = 4.53;
+let GBBigMacPrice = 3.59;
+let AUSBigMacPrice = 6.40;
+let RUSBigMacPrice = 135;
 
-// Big Mac Index
-const BMIndexUS = ((USDUSDIndex - USipp) / USipp);
-const BMIndexGB = ((GBPUSDIndex - GBipp) / GBipp);
-const BMIndexCH = ((CHFUSDIndex - CHipp) / CHipp);
-const BMIndexJAP = ((JPYUSDIndex - JAPipp) / JAPipp);
-const BMIndexEUR = ((EURUSDIndex - EURipp) / EURipp);
+let EURtoUSD = 1.16
+let GBPtoUSD = 1.35
+let AUDtoUSD = 0.74
+let RUBtoUSD = 0.014
 
-//Big Mac Index expressed in percentage
-const BMIndexUSpercent = (BMIndexUS * 100);
-const BMIndexGBpercent = (BMIndexGB * 100);
-const BMIndexCHpercent = (BMIndexCH * 100);
-const BMIndexJAPpercent = (BMIndexJAP * 100);
-const BMIndexEURpercent = (BMIndexEUR * 100);
-
-//Big Mac Index comparison between countries
-const BMIndexComparisonUS = (BMIndexUSpercent > BMIndexUSpercent);
-const BMIndexComparisonGB = (BMIndexGBpercent > BMIndexUSpercent);
-const BMIndexComparisonCH = (BMIndexCHpercent > BMIndexUSpercent);
-const BMIndexComparisonJAP = (BMIndexJAPpercent > BMIndexUSpercent);
-const BMIndexComparisonEUR = (BMIndexEURpercent > BMIndexUSpercent);
-
-//Logged variables results
-console.log(BMIndexUSpercent);
-console.log(BMIndexGBpercent);
-console.log(BMIndexCHpercent);
-console.log(BMIndexJAPpercent);
-console.log(BMIndexEURpercent);
-
-console.log(BMIndexComparisonUS);
-console.log(BMIndexComparisonGB);
-console.log(BMIndexComparisonCH);
-console.log(BMIndexComparisonJAP);
-console.log(BMIndexComparisonEUR);
+function impliedPurchasingPower (USBigMacPrice, anotherPrice) {
+    let ipp = USBigMacPrice / anotherPrice
+    return ipp
+}
---
+++ readme.md
@@ -0,0 +1,26 @@
+Today's exercise: Big Mac Index
+
+Repo: boolean-uk-js-big-mac-index
+
+Description
+Today we're focusing on the basic blocks we've learned this morning: Expressions, Numbers and Variables. 
+
+For that, we're going to build a calculator. But not any calculator, a Big Mac Index Calculator!
+Learn more about it here => https://en.wikipedia.org/wiki/Big_Mac_Index
+
+Instructions
+- Use this template as a starting point => https://codesandbox.io/s/big-mac-index-mcrc8?from-embed
+- Use the following US Big Mac price as the base currency => US$5.66
+- Calculate the index of 5 other countries
+- Output each result to the console
+
+Tips
+- Before writing any code, think about how you translate the math into code
+
+Challenge
+Instead of outputting the index, can you output a boolean that compares two indices that you previously calculated?
+Do this for each index you've created before.
+
+Challenge 2
+No one likes to look at just numbers! Could you find a way to include useful text in each output?
+Use your Google-Fu to find a way to include some useful information to each of the values you're logging!
